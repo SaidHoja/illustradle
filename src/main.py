7fe95457eaed4de33c532d5704489f3688716fd5
Max Rountree <max.r.rountree@gmail.com>
2025-03-29 17:06:09 -0400

    Going to get rid of spacy

--- src/main.py
+++ src/main.py
@@ -1,43 +1,37 @@
-import spacy
-import numpy as np
+# import spacy
+# import numpy as np
 
 from flask import Flask, render_template, request
 
 print("Initializing Flask...")
 app = Flask(__name__)
 
-print("Initializing spaCy...")
-nlp_en = spacy.load("en_core_web_md")
+# model_name = "en_core_web_lg"
+# print(f"Initializing spaCy ({model_name})...")
+# nlp_en = spacy.load(model_name)
 
 category_of_the_day = "animals"
 word_of_the_day = "horse"
-word_of_the_day_nlp = nlp_en(word_of_the_day)
+# word_of_the_day_nlp = nlp_en(word_of_the_day)
 
 
 # https://medium.com/data-science/how-to-build-a-fast-most-similar-words-method-in-spacy-32ed104fe498
-def most_similar(word, topn=5):
-    word = nlp_en.vocab[str(word)]
-    queries = [
-        w
-        for w in word.vocab
-        if w.is_lower == word.is_lower and w.prob >= -15 and np.count_nonzero(w.vector)
-    ]
-
-    by_similarity = sorted(queries, key=lambda w: word.similarity(w), reverse=True)
-    return [
-        (w.lower_, w.similarity(word))
-        for w in by_similarity[: topn + 1]
-        if w.lower_ != word.lower_
-    ]
+# def most_similar(in_word: str, topn: int = 5) -> list[str]:
+#     word = nlp_en.vocab[in_word]
+#     similarities = []
+#     for w in nlp_en.vocab.strings:
+#         if w.is_lower():
+#             similarities.append((w.text, w.similarity(word)))
+#     similarities.sort(key=lambda a: a[1], reverse=False)
+#     return similarities
 
 
-print(f"Computing similar words to {word_of_the_day}...")
-most_similar_words = most_similar(word_of_the_day, topn=20)
-print(most_similar_words)
+# print(f"Computing similar words to {word_of_the_day}...")
+# print(most_similar(word_of_the_day), 20)
 
 
-def test_word(word: str) -> float:
-    return word_of_the_day_nlp.similarity(nlp_en(word))
+# def test_word(word: str) -> float:
+#     return word_of_the_day_nlp.similarity(nlp_en(word))
 
 
 # Frontend routes
@@ -51,11 +45,11 @@ def welcome():
 @app.route("/api/guess", methods=["POST"])
 def guess():
     user_guess = request.form["guess"]
-    similarity = test_word(request.form["guess"])
+    # similarity = test_word(request.form["guess"])
 
     params = {
         "guess": request.form["guess"],
-        "similarity": round(similarity * 100, 2),
+        # "similarity": round(similarity * 100, 2),
     }
     return render_template("responses/guess.html", **params)
 
