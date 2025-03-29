92b6b575e5fbdbe46d3f62ca311d0c815d524e04
Max Rountree <max.r.rountree@gmail.com>
2025-03-29 14:09:56 -0400

    Add simple form request

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
-# import the Flask library 
-from flask import Flask 
-  
-  
-# Create the Flask instance and pass the Flask 
-# constructor, the path of the correct module 
-app = Flask(__name__) 
-  
-  
-# Default route added using a decorator, for view function 'welcome' 
-# We pass a simple string to the frontend browser 
-@app.route('/') 
-def welcome(): 
-    return "Hello! Let us learn about importance of Exercise!"
-  
-  
-# Start with flask web app, with debug as True,# only if this is the starting page 
-if(__name__ == "__main__"):
+# import the Flask library
+from flask import Flask, render_template, request
+
+
+# Create the Flask instance and pass the Flask
+# constructor, the path of the correct module
+app = Flask(__name__)
+
+
+# Default route added using a decorator, for view function 'welcome'
+# We pass a simple string to the frontend browser
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+@app.route("/api/guess", methods=["POST"])
+def guess():
+    return render_template("guess.html", guess=request.form["guess"])
+
+
+# Start with flask web app, with debug as True,# only if this is the starting page
+if __name__ == "__main__":
     app.run(debug=True)
