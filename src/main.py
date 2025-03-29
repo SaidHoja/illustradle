97384c9195058b0db1e37d095533889e14d9911e
Max Rountree <max.r.rountree@gmail.com>
2025-03-29 15:28:41 -0400

    Add templates

--- src/main.py
+++ src/main.py
@@ -1,24 +1,56 @@
-# import the Flask library
+import spacy
+
 from flask import Flask, render_template, request
 
-
-# Create the Flask instance and pass the Flask
-# constructor, the path of the correct module
+print("Initializing Flask...")
 app = Flask(__name__)
 
+print("Initializing spaCy...")
+nlp = spacy.load("en_core_web_lg")
 
-# Default route added using a decorator, for view function 'welcome'
-# We pass a simple string to the frontend browser
+category_of_the_day = "animals"
+word_of_the_day = "horse"
+word_of_the_day_nlp = nlp(word_of_the_day)
+
+
+def test_word(word: str) -> float:
+    return word_of_the_day_nlp.similarity(nlp(word))
+
+
+# Frontend routes
 @app.route("/")
 def welcome():
-    return render_template("index.html")
+    params = {"category": category_of_the_day, "letters": "_" * len(word_of_the_day)}
+    return render_template("pages/index.html", **params)
 
 
+# API routes
 @app.route("/api/guess", methods=["POST"])
 def guess():
-    return render_template("guess.html", guess=request.form["guess"])
+    user_guess = request.form["guess"]
+
+    similarity = test_word(request.form["guess"])
+
+    print(similarity)
+
+    params = {
+        "guess": request.form["guess"],
+        "similarity": round(similarity * 100, 2),
+    }
+    return render_template("responses/guess.html", **params)
 
 
-# Start with flask web app, with debug as True,# only if this is the starting page
+@app.route("/api/hint", methods=["GET"])
+def hint():
+    user_best_guess = request.args.get("best_guess")
+    # Do lookup on `user_guess` and find the next best one
+    better_guess = "pig"
+
+    params = {"guess": better_guess, "letters": "_o___"}
+    return render_template("responses/hint.html", **params)
+
+
+# Start with flask web app in debug mode
+# TODO: `argparse` for `debug` flag`
 if __name__ == "__main__":
     app.run(debug=True)
